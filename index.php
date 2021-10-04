6e45a2abd598240f24cb257c8d8971972b195515
MauricioSG <udcarkangel@gmail.com>
2021-10-04 18:22:38 -0500

    Web Finished

---
+++ index.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>
+            Inicio - Coca Cola
+        </title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+        <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/35d556d216.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <a href="index.html">
+                    <img src="img/logo.svg" alt="" class="logo">
+                </a>
+                <nav>
+                    <a href="#inicio">Inicio</a>
+                    <a href="#nosotros">Nosotros</a>
+                    <a href="#servicios">Servicios</a>
+                    <a href="#galeria">Galería</a>
+                    <a href="#contactenos">Contáctenos</a>
+                </nav>
+                <a href="#" class="hamb"><i class="fas fa-bars"></i></a>
+            </div>
+        </header>
+        <main>
+            <section id="inicio">
+                <img src="img/bannerprincipal.jpg" alt="">
+                <div class="bloque-inicio">
+                    <h1>Bienvenidos a Coca Cola</h1>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                        
+                    </p>
+                    <a href="#nosotros" class="boton">Ver más</a>
+                </div>
+            </section>
+            <section id="nosotros" class="seccion">
+                <div class="container">
+                    <p>
+                        Necessitatibus quidem molestiae quis nesciunt aut a tempora 
+                        similique aspernatur quaerat, officia animi neque illum 
+                        voluptatem saepe omnis eligendi dignissimos? Nulla, porro?
+                        quidem molestiae quis nesciunt aut a tempora 
+                        similique aspernatur quaerat, officia animi neque illum 
+                        voluptatem saepe omnis eligendi dignissimos? Nulla,
+                    </p>
+                </div>
+            </section>
+            <section id="servicios" class="seccion">
+                <div class="container">
+                    <div class="row">
+                        <div class="columna columna-33 columna-mobile-100">
+                            <div class="bloque-servicio">
+                                <div class="bloque-img-servicio cuadrado-perfecto">
+                                    <img src="img/servicio1.jpg" alt="">
+                                </div>
+                                <div class="bloque-contenido-servicio">
+                                    <h3>Servicio 1</h3>
+                                    <p>
+                                        Necessitatibus quidem molestiae quis nesciunt aut a tempora 
+                                        similique aspernatur quaerat, officia animi neque illum 
+                                        voluptatem saepe omnis eligendi dignissimos? Nulla, porro?
+                                        quidem molestiae quis nesciunt aut a tempora.
+                                    </p>
+                                    <a href="#" class="boton boton-blanco">Ver más</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="columna columna-33 columna-mobile-100">
+                            <div class="bloque-servicio">
+                                <div class="bloque-img-servicio cuadrado-perfecto">
+                                    <img src="img/servicio2.jpg" alt="">
+                                </div>
+                                <div class="bloque-contenido-servicio">
+                                    <h3>Servicio 2</h3>
+                                    <p>
+                                        Necessitatibus quidem molestiae quis nesciunt aut a tempora 
+                                        similique aspernatur quaerat, officia animi neque illum 
+                                        voluptatem saepe omnis eligendi dignissimos? Nulla, porro?
+                                        quidem molestiae quis nesciunt aut a tempora.
+                                    </p>
+                                    <a href="#" class="boton boton-blanco">Ver más</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="columna columna-33 columna-mobile-100">
+                            <div class="bloque-servicio">
+                                <div class="bloque-img-servicio cuadrado-perfecto">
+                                    <img src="img/servicio3.jpg" alt="">
+                                </div>
+                                <div class="bloque-contenido-servicio">
+                                    <h3>Servicio 3</h3>
+                                    <p>
+                                        Necessitatibus quidem molestiae quis nesciunt aut a tempora 
+                                        similique aspernatur quaerat, officia animi neque illum 
+                                        voluptatem saepe omnis eligendi dignissimos? Nulla, porro?
+                                        quidem molestiae quis nesciunt aut a tempora.
+                                    </p>
+                                    <a href="#" class="boton boton-blanco">Ver más</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="galeria">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio1.jpg" alt="">
+                                <h4>Imagen1</h4>
+                            </div>
+                        </div>
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio2.jpg" alt="">
+                                <h4>Imagen2</h4>
+                            </div>
+                        </div>
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio3.jpg" alt="">
+                                <h4>Imagen3</h4>
+                            </div>
+                        </div>
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio1.jpg" alt="">
+                                <h4>Imagen4</h4>
+                            </div>
+                        </div>
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio2.jpg" alt="">
+                                <h4>Imagen5</h4>
+                            </div>
+                        </div>
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio3.jpg" alt="">
+                                <h4>Imagen6</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="contactenos" class="seccion">
+                <iframe width="520" height="400" frameborder="0" src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;
+                 q=%20Bogot%C3%A1+(Bogota)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                </iframe>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="columna columna-33 empujar-58 columna-mobile-100 empujar-0 sinpadding-mobile">
+                            <form action="index.php" method="post">
+                                <div class="form-block">
+                                    <input type="text" name="nombre" id="" class="form-control" placeholder="Nombre">
+                                </div>
+                                <div class="form-block">
+                                    <input type="email" name="email" id="" class="form-control" placeholder="Email">
+                                </div>
+                                <div class="form-block">
+                                    <textarea name="mensaje" id="" cols="30" rows="10" placeholder="Mensaje"></textarea>
+                                </div>
+                                <div class="form-block bloque-ultimo">
+                                    <input type="submit" class="boton boton-negro" value="Enviar">
+                                </div>
+                                <?php
+                                    if($_SERVER["REQUEST_METHOD"] == "POST")
+                                    {
+                                        $nombre = $_POST["nombre"];
+                                        $email = $_POST["email"];
+                                        $mensaje = $_POST["mensaje"];
+
+                                        if(isset($nombre))
+                                        {
+                                            if(isset($email))
+                                            {
+                                                if(isset($mensaje))
+                                                {
+                                                    $para = "udcarkangel@gmail.com";
+                                                    $asunto = "Esto es una prueba";
+                                                    $cuerpo = $nombre."\n".$email."\n".$mensaje;
+                                                    $adicional = "From: noreply@angel.com";
+
+                                                    mail($para,$asunto,$cuerpo,$adicional);
+
+                                                ?>
+                                                    <p>Envio exitoso</p>
+                                                <?php                                                    
+                                                }
+                                            }
+                                        }
+                                    }
+                                ?>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                
+            </section>
+        </main>
+        <footer class="seccion">
+            <div class="container">
+                <div class="row">
+                    <div class="columna columna-25 columna-mobile-100">
+                        <img src="/img/logo-blanco.png" alt="" class="logo-footer">
+                        <p>
+                            Necessitatibus quidem molestiae quis nesciunt aut a tempora 
+                            similique quidem molestiae.
+                        </p>
+                    </div>
+                    <div class="columna columna-25 columna-mobile-100">
+                        <h3>
+                            Temas relacionados
+                        </h3>
+                        <ul>
+                            <li><a href="#">Tema 1</a></li>
+                            <li><a href="#">Tema 2</a></li>
+                            <li><a href="#">Tema 3</a></li>
+                        </ul>
+                    </div>
+                    <div class="columna columna-25 columna-mobile-100">
+                        <h3>
+                            Datos de Contacto
+                        </h3>
+                        <ul>
+                            <li>cocacola@coca.com</li>
+                            <li>+5798562145</li>
+                            <li>Av. Siempre Viva 123 - Springfield <br> Estados Unidos de America</li>
+                        </ul>
+                    </div>
+                    <div class="columna columna-25 columna-mobile-100">
+                        <h3>
+                            Redes Sociales
+                        </h3>
+                        <ul class="redes">
+                            <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+                            <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram-square"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="barra-footer">
+                &copy; Derechos Reservados - 2021
+            </div>
+        </footer>
+        <script src="js/jquery.js"></script>
+        <script src="js/funciones.js"></script>
+    </body>
+</html>
